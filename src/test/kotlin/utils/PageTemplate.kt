89c6fe62bc5c69479ae6ea9d28e51a14675700f6
Alessio Coser <alessio.coser@xpeppers.com>
2018-10-31 14:21:07 +0100

    Test utility for templating

---
+++ src/test/kotlin/utils/PageTemplate.kt
@@ -0,0 +1,19 @@
+package utils
+
+import freemarker.template.Configuration
+import java.io.File
+import java.io.StringWriter
+import java.util.*
+
+class PageTemplate {
+    fun contentOf(name: String, model: HashMap<String, Any>): String {
+        val configuration = Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS)
+        configuration.setDirectoryForTemplateLoading(File("src/main/resources/templates"))
+        val template = configuration.getTemplate(name + ".ftl")
+
+        val writer = StringWriter()
+        template.process(model, writer)
+
+        return writer.toString()
+    }
+}
