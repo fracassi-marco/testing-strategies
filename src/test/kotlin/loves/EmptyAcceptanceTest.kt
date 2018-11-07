eb4970dd210aa99da2a796c7f0cd9f613c4d60a7
Marco Fracassi <fracassi.marco@gmail.com>
2018-11-07 12:14:53 +0100

    Add empty acceptance test

---
+++ src/test/kotlin/loves/EmptyAcceptanceTest.kt
@@ -0,0 +1,34 @@
+package loves
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EmptyAcceptanceTest {
+
+    @LocalServerPort
+    private var port: Int = 0
+    private lateinit var browser: PhantomJSDriver
+
+    @Before
+    fun before() {
+        browser = PhantomJSDriver()
+    }
+
+    @After
+    fun after() {
+        browser.close()
+    }
+
+    @Test
+    fun shouldFindALyrics() {
+        browser.get("http://localhost:$port/")
+    }
+}
