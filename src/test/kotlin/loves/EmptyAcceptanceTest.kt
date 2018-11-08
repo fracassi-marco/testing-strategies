1c336718ae1152222740116937c603afdb064284
Marco Fracassi <fracassi.marco@gmail.com>
2018-11-08 15:04:02 +0100

    Add WebDriverManager

--- src/test/kotlin/loves/EmptyAcceptanceTest.kt
+++ src/test/kotlin/loves/EmptyAcceptanceTest.kt
@@ -1,5 +1,6 @@
 package loves
 
+import io.github.bonigarcia.wdm.WebDriverManager
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -8,6 +9,7 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
+
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,7 +21,8 @@ class EmptyAcceptanceTest {
 
     @Before
     fun before() {
-        browser = PhantomJSDriver()
+        WebDriverManager.phantomjs().setup();
+        browser = PhantomJSDriver();
     }
 
     @After
