dda440ae0969e45c27852f9987dcb67ff144ca99
Simone Oprandi <simone.oprandi@cgm.com>
2018-11-09 16:41:24 +0100

    acceptance test

--- src/test/kotlin/loves/EmptyAcceptanceTest.kt
+++ src/test/kotlin/loves/EmptyAcceptanceTest.kt
@@ -1,10 +1,13 @@
 package loves
 
 import io.github.bonigarcia.wdm.WebDriverManager
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assumptions.assumeThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.openqa.selenium.By
 import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
@@ -32,6 +35,32 @@ class EmptyAcceptanceTest {
 
     @Test
     fun shouldFindALyrics() {
+        caricoLaPagina()
+        inseriscoAutore("883")
+        inseriscoTitoloCanzone("nella notte")
+        cliccoCerca()
+        verificoIlTestoDellaCanzone("tacchi a spillo")
+    }
+
+    private fun caricoLaPagina() {
         browser.get("http://localhost:$port/")
     }
+
+    private fun inseriscoAutore(autore: String) {
+        browser.findElement(By.id("txtAutore")).sendKeys(autore)
+    }
+
+    private fun inseriscoTitoloCanzone(titolo:   String) {
+        browser.findElement(By.id("txtTitolo")).sendKeys(titolo)
+    }
+
+    private fun cliccoCerca() {
+        browser.findElement(By.id("btnCerca")).click()
+    }
+
+    private fun verificoIlTestoDellaCanzone(testo: String) {
+        assumeThat(browser.pageSource).containsIgnoringCase(testo)
+    }
+
+
 }
